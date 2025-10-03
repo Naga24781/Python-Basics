8c83ee4d41d0f44dae41393ef4523cddf100bcc6
Naga24781 <devireddynagasurendra@gmail.com>
2025-10-03 05:27:22 +0100

    Day 3: Loop practice programs

---
+++ reverse_count.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+
+while n > 0:
+    print("The reverse value is", n)
+    n -= 1
---
+++ sum_even_odd.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number:"))
+total =0
+for i in range(2,n+1,2):
+    total+=i
+    print("Total is:",total)
+
+n=int(input("enter a number:"))
+total =0
+for i in range(1,n+1,2):
+    total+=i
+    print("Total is:",total) saving this file
